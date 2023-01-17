4ada43663c8ae21eb58784541fd1f1034f6014a5
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-17 16:19:19 +0600

    upload

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+const url = document.getElementById("url");
+const btn = document.getElementById("btn");
+let QRCodeDiv = document.getElementById("qrcode");
+
+btn.onclick = () => {
+  QRCodeDiv.innerHTML = "";
+  QRCodeDiv.classList.remove("p-8");
+  QRCodeDiv.parentElement.classList.remove(
+    "my-3",
+    "px-4",
+    "border",
+    "rounded-md"
+  );
+  if (!url.value) {
+    return false;
+  }
+  QRCodeDiv.classList.add("p-8");
+  const img = document.createElement("img");
+  img.classList.add("mx-auto");
+
+  /***
+   * Use API
+   * website: https://goqr.me/api/
+   * Free API
+   */
+  img.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${url.value}`;
+  QRCodeDiv.appendChild(img);
+};
